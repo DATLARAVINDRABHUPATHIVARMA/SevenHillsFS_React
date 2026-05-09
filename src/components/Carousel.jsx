cea812288ea60fb233a3d9a6853daf632d264bc4
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-05-09 06:23:04 -0400

    Not reviewed Carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -19,7 +19,7 @@ import twelfthImage from "../assets/SliderImages/12.jpg";
 const Carousel = () => {
   return (
     <div>
-      <Swiper modules={[Autoplay, EffectFade]} effect="fade" fadeEffect={{ crossFade: true }} loop={true} autoplay={{ delay: 4000, disableOnInteraction: false }} slidesPerView={1} speed={2000}>
+      <Swiper modules={[Autoplay, EffectFade]} effect="fade" fadeEffect={{ crossFade: true }} loop={true} autoplay={{ delay: 3000, disableOnInteraction: false }} slidesPerView={1} speed={1500}>
         <SwiperSlide>
           <div className="relative h-[70vh] lg:h-[90vh] overflow-hidden">
             <div className="relative h-full w-full">
@@ -28,18 +28,8 @@ const Carousel = () => {
             </div>
             <div className="absolute inset-0 flex items-center text-white justify-start max-w-7xl mx-auto px-4 lg:px-0">
               <div className="max-w-lg px-12 lg:px-0">
-                <h1 className="text-2xl lg:text-5xl font-bold mb-4">
-                  Professional Facility Management Services in{" "}
-                  <span className="text-sky-500">Hyderabad</span>
-                </h1>
-                <p className="text-sm lg:text-xl">
-                  As a leading provider of comprehensive facility management
-                  services, we are dedicated to delivering exceptional solutions
-                  that enhance the efficiency, safety, and sustainability of
-                  your facilities. With our expertise and commitment to
-                  excellence, we strive to exceed your expectations and ensure
-                  the smooth operation of your facilities.
-                </p>
+                <h1 className="text-2xl lg:text-5xl font-bold mb-4">Professional Facility Management Services in{" "}<span className="text-sky-500">Hyderabad</span></h1>
+                <p className="text-sm lg:text-xl">As a leading provider of comprehensive facility management services, we are dedicated to delivering exceptional solutions that enhance the efficiency, safety, and sustainability of your facilities. With our expertise and commitment to excellence, we strive to exceed your expectations and ensure the smooth operation of your facilities.</p>
               </div>
             </div>
           </div>
@@ -70,22 +60,13 @@ const Carousel = () => {
         <SwiperSlide>
           <div className="relative h-[70vh] lg:h-[90vh] overflow-hidden">
             <div className="relative h-full w-full">
-              <img
-                src={thirdImage}
-                alt="image 1"
-                className="h-full w-full object-cover"
-              />
+              <img src={thirdImage} alt="image 1" className="h-full w-full object-cover"/>
               <div className="absolute inset-0 bg-gradient-to-r from-black via-sky-900/50 to-sky-900/10"></div>
             </div>
             <div className="absolute inset-0 flex items-center text-white justify-start max-w-7xl mx-auto px-4 lg:px-0">
               <div className="max-w-lg px-12 lg:px-0">
-                <h1 className="text-2xl lg:text-5xl font-bold mb-4">
-                  Professional Facility Management Services in{" "}
-                  <span className="text-sky-500">Andhra Pradesh</span>
-                </h1>
-                <p className="text-sm lg:text-xl">
-                  Facility management services encompass a wide range of activities aimed at ensuring the efficient and effective operation of facilities. These services include maintenance, cleaning, security, landscaping, waste management, and more. By outsourcing facility management services to a professional provider, organizations can focus on their core business activities while ensuring that their facilities are well-maintained and operate smoothly.
-                </p>
+                <h1 className="text-2xl lg:text-5xl font-bold mb-4">Professional Facility Management Services in{" "}<span className="text-sky-500">Andhra Pradesh</span></h1>
+                <p className="text-sm lg:text-xl">Facility management services encompass a wide range of activities aimed at ensuring the efficient and effective operation of facilities. These services include maintenance, cleaning, security, landscaping, waste management, and more. By outsourcing facility management services to a professional provider, organizations can focus on their core business activities while ensuring that their facilities are well-maintained and operate smoothly.</p>
               </div>
             </div>
           </div>
@@ -129,12 +110,7 @@ const Carousel = () => {
                   Professional Facility Management Services in{" "}
                   <span className="text-sky-500">TS</span>
                 </h1>
-                <p className="text-sm lg:text-xl">
-                  We are a leading provider of comprehensive facility management
-                  services, dedicated to delivering exceptional solutions that
-                  enhance the efficiency, safety, and sustainability of your
-                  facilities.
-                </p>
+                <p className="text-sm lg:text-xl">We are a leading provider of comprehensive facility management services,   delivering exceptional solutions that enhance the efficiency, safety, and sustainability of your facilities.</p>
               </div>
             </div>
           </div>
@@ -204,15 +180,9 @@ const Carousel = () => {
             <div className="absolute inset-0 flex items-center text-white justify-start max-w-7xl mx-auto px-4 lg:px-0">
               <div className="max-w-lg px-12 lg:px-0">
                 <h1 className="text-2xl lg:text-5xl font-bold mb-4">
-                  Professional Facility Management Services in{" "}
-                  <span className="text-sky-500">Turkapally</span>
+                  Professional Facility Management Services in{" "}<span className="text-sky-500">Turkapally</span>
                 </h1>
-                <p className="text-sm lg:text-xl">
-                  We are a leading provider of comprehensive facility management
-                  services, dedicated to delivering exceptional solutions that
-                  enhance the efficiency, safety, and sustainability of your
-                  facilities.
-                </p>
+                <p className="text-sm lg:text-xl">We are a leading provider of comprehensive facility management services, dedicated to delivering exceptional solutions that enhance the efficiency, safety, and sustainability of your facilities.</p>
               </div>
             </div>
           </div>
@@ -233,12 +203,7 @@ const Carousel = () => {
                   Professional Facility Management Services in{" "}
                   <span className="text-sky-500">Shamshabad</span>
                 </h1>
-                <p className="text-sm lg:text-xl">
-                  We are a leading provider of comprehensive facility management
-                  services, dedicated to delivering exceptional solutions that
-                  enhance the efficiency, safety, and sustainability of your
-                  facilities.
-                </p>
+                <p className="text-sm lg:text-xl">We are a leading provider of comprehensive facility management services, dedicated to delivering exceptional solutions that enhance the efficiency, safety, and sustainability of your facilities.</p>
               </div>
             </div>
           </div>
@@ -259,12 +224,7 @@ const Carousel = () => {
                   Professional Facility Management Services in{" "}
                   <span className="text-sky-500">Chandan Veli</span>
                 </h1>
-                <p className="text-sm lg:text-xl">
-                  We are a leading provider of comprehensive facility management
-                  services, dedicated to delivering exceptional solutions that
-                  enhance the efficiency, safety, and sustainability of your
-                  facilities.
-                </p>
+                <p className="text-sm lg:text-xl">We are a leading provider of comprehensive facility management services, dedicated to delivering exceptional solutions that enhance the efficiency, safety, and sustainability of your facilities.</p>
               </div>
             </div>
           </div>
@@ -272,11 +232,7 @@ const Carousel = () => {
         <SwiperSlide>
           <div className="relative h-[70vh] lg:h-[90vh] overflow-hidden">
             <div className="relative h-full w-full">
-              <img
-                src={ninthImage}
-                alt="image 2"
-                className="h-full w-full object-cover"
-              />
+              <img src={ninthImage} alt="image 2" className="h-full w-full object-cover"/>
               <div className="absolute inset-0 bg-gradient-to-r from-black via-sky-900/50 to-sky-900/10"></div>
             </div>
             <div className="absolute inset-0 flex items-center text-white justify-start max-w-7xl mx-auto px-4 lg:px-0">
