ea98188f80d98ae74fa9e9a812886252667146d5
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-05-14 06:06:07 -0400

    Testimonial Heading

--- src/components/ClientsFeed.jsx
+++ src/components/ClientsFeed.jsx
@@ -44,10 +44,14 @@ const reviews = [
 const ClientsFeed = () => {
   return (
     <div className="py-20 bg-gray-100">
+      <div>
+        <h2 className="text-3xl font-bold text-center mb-8">What Our Clients Say</h2>
+        <p className="text-center text-gray-600 mb-12">Hear what our satisfied clients have to say about their experience working with Seven Hills Facility Services.</p>
+      </div>
       <Swiper modules={[Autoplay, Pagination]} loop={true} autoplay={{ delay: 2000, disableOnInteraction: false }} slidesPerView={1} speed={800} breakpoints={{ 640: { slidesPerView: 2 }, 1025: { slidesPerView: 4 } }}>
         {reviews.map((review) => (
           <SwiperSlide key={review.id}>
-            <div className="rel bg-white p-6 m-4 h-full flex flex-col justify-between border border-sky-500 border-2">
+            <div className="relative bg-white p-6 m-4 h-full flex flex-col justify-between border border-sky-500 border-2">
               <FaQuoteLeft className="absolute top-4 right-4 text-sky-500 text-3xl" />
               <div className="flex items-center space-x-4 mb-2">
                 <FaUserCircle className="text-4xl text-sky-500"/>
