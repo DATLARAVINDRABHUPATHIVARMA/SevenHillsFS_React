93972df1130e5ed588d28dcd3c9512619d4cc7ef
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-05-19 01:31:51 -0400

    Main Branch making Live

--- src/components/Choose.jsx
+++ src/components/Choose.jsx
@@ -8,7 +8,7 @@ const Choose = () => {
         <div className="flex flex-col lg:flex-row">
           <div className="w-full lg:w-1/2">
             <h2 className="text-3xl font-bold mb-4 px-4 lg:px-0">Why Choose <span className="text-sky-500">Seven Hills?</span></h2>
-            <p className="max-w-lg px-4 lg:px-0">We are a leading facility management company dedicated to providing exceptional services to our clients. With years of experience in the industry, we have built a reputation for excellence and reliability. Our team of skilled professionals is committed to delivering top-quality solutions tailored to meet your specific needs. We pride ourselves on our attention to detail, customer satisfaction, and our ability to handle a wide range of facility management tasks efficiently and effectively.</p>
+            <p className="max-w-lg px-4 lg:px-0">We are a leading facility management company dedicated to providing exceptional services to our clients. With years of experience in the industry, we have built a reputation for excellence and reliability. Our team of skilled professionals is committed to delivering top-quality solutions tailored to meet your specific needs. We pride ourselves on our attention to detail, customer satisfaction, and our ability to handle a wide range of facility management tasks efficiently.</p>
             <div className="px-4 lg:px-0 mt-8 mb-6">
               <h3 className="text-xl font-bold">Trained Professionals</h3>
               <p className="text-xs">Our team consists of highly trained and certified professionals who are dedicated to providing top-notch service and support.</p>
