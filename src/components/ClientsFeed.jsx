c2ef15457f420506c27ffe9945bd2a9cd1239e5f
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-07-24 02:18:22 -0400

    Swayam

--- src/components/ClientsFeed.jsx
+++ src/components/ClientsFeed.jsx
@@ -23,13 +23,15 @@ const reviews = [
     id: 3,
     name: "Emily Johnson",
     position: "Director, BlueDart Express Ltd.",
-    review: "A pleasure to work with. They exceeded our expectations in every way.",
+    review:
+      "A pleasure to work with. They exceeded our expectations in every way.",
   },
   {
     id: 4,
     name: "Michael Brown",
     position: "Owner, V.R.K Jewellers",
-    review: "Outstanding quality and service. I would definitely recommend them to others.",
+    review:
+      "Outstanding quality and service. I would definitely recommend them to others.",
   },
   {
     id: 5,
@@ -41,42 +43,58 @@ const reviews = [
     id: 6,
     name: "David Wilson",
     position: "Operations Director, JLL India Pvt. Ltd.",
-    review: "Reliable and efficient service. They truly understand our needs and exceed expectations.",
+    review:
+      "Reliable and efficient service. They truly understand our needs and exceed expectations.",
   },
   {
     id: 7,
     name: "Lisa Anderson",
     position: "HR Director, Megha Engineering & Infrastructures Ltd.",
-    review: "Excellent customer service and attention to detail. They go above and beyond to ensure our satisfaction.",
+    review:
+      "Excellent customer service and attention to detail. They go above and beyond to ensure our satisfaction.",
   },
   {
     id: 8,
     name: "Robert Taylor",
     position: "Consultant, GMR Group",
-    review: "Impressive results and outstanding professionalism. A true partner in our success.",
+    review:
+      "Impressive results and outstanding professionalism. A true partner in our success.",
   },
   {
     id: 9,
     name: "Jessica Martinez",
     position: "Facilities Manager, Coromandel International Ltd.",
-    review: "Their team is responsive, reliable, and always delivers high-quality work. We are extremely satisfied with their services.",
-  }
+    review:
+      "Their team is responsive, reliable, and always delivers high-quality work. We are extremely satisfied with their services.",
+  },
 ];
 
 const ClientsFeed = () => {
   return (
     <div className="py-20 bg-gray-100">
       <div>
-        <h2 className="text-3xl font-bold text-center mb-8">What Our Clients Say</h2>
-        <p className="text-center text-gray-600 mb-12">Hear what our satisfied clients have to say about their experience working with Seven Hills Facility Services.</p>
+        <h2 className="text-3xl font-bold text-center mb-8">
+          What Our Clients Say
+        </h2>
+        <p className="text-center text-gray-600 mb-12">
+          Hear what our satisfied clients have to say about their experience
+          working with Seven Hills Facility Services.
+        </p>
       </div>
-      <Swiper modules={[Autoplay, Pagination]} loop={true} autoplay={{ delay: 2000, disableOnInteraction: false }} slidesPerView={1} speed={800} breakpoints={{ 640: { slidesPerView: 2 }, 1025: { slidesPerView: 4 } }}>
+      <Swiper
+        modules={[Autoplay, Pagination]}
+        loop={true}
+        autoplay={{ delay: 2000, disableOnInteraction: false }}
+        slidesPerView={1}
+        speed={800}
+        breakpoints={{ 640: { slidesPerView: 2 }, 1025: { slidesPerView: 4 } }}
+      >
         {reviews.map((review) => (
           <SwiperSlide key={review.id}>
             <div className="relative bg-white p-6 m-4 h-full flex flex-col justify-between border border-sky-500 border-2">
               <FaQuoteLeft className="absolute top-4 right-4 text-sky-500 text-3xl" />
               <div className="flex items-center space-x-4 mb-2">
-                <FaUserCircle className="text-4xl text-sky-500"/>
+                <FaUserCircle className="text-4xl text-sky-500" />
                 <div className="flex flex-col justify-start items-start">
                   <div className="font-bold text-gray-900">{review.name}</div>
                   <div className="text-gray-600 text-xs">{review.position}</div>
